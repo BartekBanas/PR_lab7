14d1d853a9b20e65af5f4fcfc9bd0f7361114489
Bartek Banaś <bartek.banas675@gmail.com>
2022-11-26 16:26:39 +0100

    Should be nothing

--- src/Simple_executor_test.java
+++ src/Simple_executor_test.java
@@ -6,34 +6,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Simple_executor_test {
-    private static final int NTHREADS = 10;
-    private static final double dx = 0.001;
+    private static final int NTHREADS = 2;
+    private static final int NTASKS = 10;
+    private static final double dx = 0.00001;
 
 
     public static void main(String[] args) {
         double a = 0;
         double b = Math.PI;
 
-        //sekwencyjnie (0.0)
+        //sekwencyjnie
         Calka_callable sequentialResult = new Calka_callable(a, b, dx);
         System.out.println("\nSequential Result: " + sequentialResult.compute_integral() + "\n");
 
-        //calka podzial na watki, basic (3.0)
         List<Future<Double>> partialResults = new ArrayList<>();
 
-        ExecutorService executor = Executors.newFixedThreadPool(NTHREADS);
 
-        double fragment = (b - a) / NTHREADS;
-        for (int i = 0; i < NTHREADS; i++) {
+        ExecutorService executor;
+        executor = Executors.newFixedThreadPool(NTHREADS);
+
+
+        double fragment = (b - a) / NTASKS;
+        for (int i = 0; i < NTASKS; i++) {
             partialResults.add(executor.submit(new Calka_callable(i * fragment, (i + 1) * fragment, dx)));
         }
 
         double parallelResult = 0;
 
-        for (Future<Double> r : partialResults) {
+        for (Future<Double> partialResult : partialResults) {
             try {
-                parallelResult += r.get();
-            } catch (InterruptedException | ExecutionException e) {
+                parallelResult += partialResult.get();
+            } catch (ExecutionException | InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
@@ -53,8 +56,8 @@ public class Simple_executor_test {
         // Wait until all threads finish
         while (!executor.isTerminated()) {}
 
-        System.out.println("Finished all threads");
+        System.out.println("\nFinished all threads");
         System.out.format("\nCounter_1: %d, Counter_2 %d\n\n",
                 counter.get_c1(), counter.get_c2());
     }
-} 
+}
